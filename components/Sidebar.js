10b81f01a44a716c9db8066f14d114eade9ea0ca
saddamkhan2036 <saddamkhan.2036@gmail.com>
2022-10-24 17:15:40 +0400

    create feed component and its header

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from "react";
 import SidebarMenuItem from "./SidebarMenuItem";
 import { HomeIcon } from "@heroicons/react/solid";
 import {
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
@@ -14,7 +15,7 @@ export default function Home() {
         {/* Sidebar */}
         <Sidebar />
         {/* Feed */}
-
+        <Feed />
         {/* widgets */}
 
         {/* Model */}
