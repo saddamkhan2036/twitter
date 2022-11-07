1bc4cd279925bb80c6b9dcc7f66b5e39693152a4
saddamkhan2036 <saddamkhan.2036@gmail.com>
2022-11-07 23:10:09 +0400

    add like functionality to the post

--- components/Post.js
+++ components/Post.js
@@ -6,8 +6,52 @@ import {
   ShareIcon,
   TrashIcon,
 } from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  setDoc,
+} from "firebase/firestore";
 import Moment from "react-moment";
+import { db } from "../firebase";
+import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { info } from "autoprefixer";
+
 export default function Post({ post }) {
+  const { data: session } = useSession();
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", post.id, "likes"),
+      (snapshot) => setLikes(snapshot.docs)
+    );
+  }, [db]);
+
+  useEffect(() => {
+    setHasLiked(
+      likes.findIndex((like) => like.id === session?.user.uid) !== -1
+    );
+  }, [likes]);
+
+  async function likePost() {
+    if (session){
+      if (hasLiked) {
+        await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid));
+      } else {
+        await setDoc(doc(db, "posts", post.id, "likes", session?.user.uid), {
+          username: session.user.username,
+        });
+      }
+    } else {
+      signIn();
+    }
+   
+  }
   return (
     <div className="flex  p-3 cursor-pointer border-b ">
       {/* user image */}
@@ -29,7 +73,9 @@ export default function Post({ post }) {
               @{post.data().username} -{" "}
             </span>
             <span className="text-sm sm:text-[15px] hover:underline">
-              <Moment fromNow ago>{post?.timestamp?.toDate()}</Moment>
+              <Moment fromNow ago>
+                {post?.data().timestamp?.toDate()}
+              </Moment>
             </span>
           </div>
           {/* dot icon */}
@@ -45,7 +91,20 @@ export default function Post({ post }) {
         <div className="flex justify-between text-gray-500 p-2 ">
           <ChatAlt2Icon className="h-9  w-9 hoverEffects p-2 hover:text-sky-500 hover:bg-sky-100" />
           <TrashIcon className="h-9 w-9 hoverEffects p-2 hover:text-red-600 hover:bg-red-100" />
-          <HeartIcon className="h-9 w-9 hoverEffects p-2 hover:text-red-600 hover:bg-red-100" />
+          <div className="flex items-center">
+            {hasLiked ? (
+              <HeartIconFilled
+                onClick={likePost}
+                className="h-9 w-9 hoverEffects p-2 text-red-600 hover:bg-red-100"
+              />
+            ) : (
+              <HeartIcon
+                onClick={likePost}
+                className="h-9 w-9 hoverEffects p-2 hover:text-red-600 hover:bg-red-100"
+              />
+            )}
+            {likes.length > 0 && <span className={`${hasLiked && "text-red-600"} text-sm select-none`}>{likes.length}</span>}
+          </div>
           <ShareIcon className="h-9 w-9 hoverEffects p-2 hover:text-sky-500 hover:bg-sky-100" />
           <ChartBarIcon className="h-9 w-9 hoverEffects p-2 hover:text-sky-500 hover:bg-sky-100" />
         </div>
