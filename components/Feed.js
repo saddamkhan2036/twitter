1e31fd3d2847c53d728946f9b2e5316b79b7432f
saddamkhan2036 <saddamkhan.2036@gmail.com>
2022-11-07 20:56:17 +0400

    get post data from firestore and show them in the post section

--- components/Feed.js
+++ components/Feed.js
@@ -1,50 +1,21 @@
 import { SparklesIcon } from "@heroicons/react/outline";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Input from "./Input";
 import Post from "./Post";
 
 export default function Feed() {
-  const posts = [
-    {
-      id: "1",
-      name: "Saddam Khan",
-      username: "yousafzai2036",
-      userImg:
-        "https://th.bing.com/th/id/OIP.WtH2O_VTIn8pQrkDDqNNAgAAAA?pid=ImgDet&rs=1",
-      img: "https://images.unsplash.com/photo-1665963112133-011965f045d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      text: "Explore exteriors from around the world — from brutalist buildings to minimalist structures that will give you a new appreciation for the art of architecture.",
-      timestamp: " 2 hours ago",
-    },
-    {
-      id: "2",
-      name: "Saddam Khan",
-      username: "yousafzai2036",
-      userImg:
-        "https://th.bing.com/th/id/OIP.WtH2O_VTIn8pQrkDDqNNAgAAAA?pid=ImgDet&rs=1",
-      img: "https://images.unsplash.com/photo-1666679643373-f2e184deefce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      text: "Through the use of unlikely textures, intriguing subject matter, and new formats — photography has the power to challenge our perspectives and push creativity forward",
-      timestamp: " 4 hours ago",
-    },
-    {
-      id: "3",
-      name: "Saddam Khan",
-      username: "yousafzai2036",
-      userImg:
-        "https://th.bing.com/th/id/OIP.WtH2O_VTIn8pQrkDDqNNAgAAAA?pid=ImgDet&rs=1",
-      img: "https://images.unsplash.com/photo-1663614169496-a379baee8795?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
-      text: "Covering the latest important events, news-worthy moments and movements from around the world — from political protests to cultural celebrations. When submitting, please provide a photo description so we understand the full context of the image",
-      timestamp: " 6 hours ago",
-    },
-    {
-      id: "3",
-      name: "Saddam Khan",
-      username: "yousafzai2036",
-      userImg:
-        "https://th.bing.com/th/id/OIP.WtH2O_VTIn8pQrkDDqNNAgAAAA?pid=ImgDet&rs=1",
-      img: "https://images.unsplash.com/photo-1661366051048-363961b03c7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      text: "Reflecting the realities of the modern workplace in their many forms — from images of remote working and start-ups to shots of engineers and artists at work",
-      timestamp: " 10 hours ago",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  useEffect(
+    ()=>
+      onSnapshot(
+      query(collection(db, "posts"),orderBy("timestamp","desc")), 
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    ), []);
+
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -54,8 +25,8 @@ export default function Feed() {
         </div>
       </div>
       <Input />
-      {posts.map((post,id) => (
-        <Post key={id} post={post} />
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
       ))}
     </div>
   );
--- components/Post.js
+++ components/Post.js
@@ -6,14 +6,14 @@ import {
   ShareIcon,
   TrashIcon,
 } from "@heroicons/react/outline";
-
+import Moment from "react-moment";
 export default function Post({ post }) {
   return (
     <div className="flex  p-3 cursor-pointer border-b ">
       {/* user image */}
       <img
         className="h-11 w-11 rounded-full mr-4"
-        src={post.userImg}
+        src={post.data().userImg}
         alt="user-img"
       />
       {/* right side */}
@@ -22,17 +22,25 @@ export default function Post({ post }) {
         <div className=" flex  items-center justify-between ">
           {/* post user info */}
           <div className="flex items-center space-x-1 whitespace-nowrap">
-            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline ">{post.name}</h4>
-            <span className="text-sm sm:text-[15px]">@{post.username} - </span>
-            <span className="text-sm sm:text-[15px] hover:underline">{post.timestamp}</span>
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline ">
+              {post.data().name}
+            </h4>
+            <span className="text-sm sm:text-[15px]">
+              @{post.data().username} -{" "}
+            </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+              <Moment fromNow ago>{post?.timestamp?.toDate()}</Moment>
+            </span>
           </div>
           {/* dot icon */}
           <DotsHorizontalIcon className="h-10 hoverEffects w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
         </div>
         {/* post text */}
-        <p className="text-gray-800  text-[15px] sm:text-[16px] mb-2">{post.text}</p>
+        <p className="text-gray-800  text-[15px] sm:text-[16px] mb-2">
+          {post.data().text}
+        </p>
         {/* post image */}
-        <img className="rounded-2xl mr-2 " src={post.img} alt="" />
+        <img className="rounded-2xl mr-2 " src={post.data().image} alt="" />
         {/* icons */}
         <div className="flex justify-between text-gray-500 p-2 ">
           <ChatAlt2Icon className="h-9  w-9 hoverEffects p-2 hover:text-sky-500 hover:bg-sky-100" />
